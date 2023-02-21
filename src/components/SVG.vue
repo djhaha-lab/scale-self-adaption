e6c61419c81e0a6d7053ba626157b7903e816512
unknown <2608211701@qq.com>
2023-02-22 07:07:48 +0800

    初始化

---
+++ src/components/SVG.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { SVG } from "@svgdotjs/svg.js";
+var draw = SVG().addTo("body").size(300, 300);
+var rect = draw.rect(100, 100).attr({ fill: "#f06" });
+</script>
+
+<template>
+  <div class="bg-black"></div>
+</template>
+
+<style scoped></style>
